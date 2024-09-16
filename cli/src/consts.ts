00212597bda1d4373e9a7bf79ce1f99b7d2465a2
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-09-16 10:06:36 +0530

    feat:prompts

---
+++ cli/src/consts.ts
@@ -0,0 +1,5 @@
+// import path from "path";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const distPath = path.dirname(__filename);
+// export const PKG_ROOT = path.join(distPath, "../");
--- cli/src/utils/getCSTVersion.ts
+++ cli/src/utils/getCSTVersion.ts
@@ -1,16 +1,15 @@
-import path from 'path';
-// import fs from "fs-extra";
-// import { type PackageJson } from "type-fest";
+import path from "path";
+import fs from "fs-extra";
+import { type PackageJson } from "type-fest";
+// import { PKG_ROOT } from "@/consts";
 
-// import { PKG_ROOT } from "~/consts.js";
-
-//TODO: get version from package.json
+//TODO:update path so that the version is fetched from build/package.json
 
 export const getVersion = () => {
-  // const packageJsonPath = path.join(PKG_ROOT, "package.json");
+  const packageJsonPath = path.join(PKG_ROOT, "package.json");
 
-  // const packageJsonContent = fs.readJSONSync(packageJsonPath) as PackageJson;
+  const packageJsonContent = fs.readJSONSync(packageJsonPath) as PackageJson;
 
-  // return packageJsonContent.version ?? "1.0.0";
-  return '1.0.0';
+  return packageJsonContent.version ?? "1.0.0";
+  return "1.0.0";
 };
