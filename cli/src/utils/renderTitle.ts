7fa7235bdb3f1c5f415090d49a03919d2e26e565
ofcljaved <javedofficialme@gmail.com>
2024-09-18 10:45:57 +0530

    chore: use font tiny in title

--- cli/src/utils/renderTitle.ts
+++ cli/src/utils/renderTitle.ts
@@ -2,6 +2,7 @@ import cfonts from "cfonts";
 import { TITLE } from "@/const";
 
 const fontOptions = {
+    font: "tiny",
     background: 'transparent',
     letterSpacing: 1,
     lineHeight: 1,
@@ -10,6 +11,6 @@ const fontOptions = {
 };
 
 export async function renderTitle(){
-    cfonts.say(TITLE.split(" ").join("|"), fontOptions);
+    cfonts.say(TITLE, fontOptions);
     await new Promise((resolve) => setTimeout(resolve, 200));
 }
