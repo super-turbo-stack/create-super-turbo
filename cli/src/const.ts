caa0369d00b33577105ab8957bcf40d81d33b3aa
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-09-19 14:06:07 +0530

    feat: added Templates for base Turbo and Next

--- cli/src/const.ts
+++ cli/src/const.ts
@@ -1,10 +1,10 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-//const __filename = fileURLToPath(import.meta.url);
-const distPath =  path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+export const distPath = path.dirname(__filename);
 export const PKG_ROOT = path.join(distPath, "../");
 
-export  const TITLE = "CREATE SUPER TURBO";
+export const TITLE = "CREATE SUPER TURBO";
 
 export const CREATE_SUPER_TURBO = "create-super-turbo";
