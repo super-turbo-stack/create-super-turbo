bd175230ba2cdd6839b40c167c7150e0ecae5f15
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-09-19 15:44:30 +0530

    fix:merge conflicts

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -19,12 +19,26 @@ async function main() {
     } = await runCli();
 
     await bootStrapTurbo();
+    //Copy the Turbo-base to user's dir
+
+    //git init if git is true
+
+    //copy react app to /apps
+    //install react packages
+
+    //copy next app to /apps
+    //install next packages
+
+    //copy express app to /apps
+    //install express packages
   } catch (err) {
+    logger.error('Aborting installation...');
     logger.error('Aborting installation...');
     if (err instanceof Error) {
       logger.error(err.message);
     } else {
       logger.error(
+        'An unknown error has occurred. Please open an issue on github with the below:'
         'An unknown error has occurred. Please open an issue on github with the below:'
       );
       console.log(err);
