80d79f9975e33cdafb99bfaf58ad98aeb55c5dca
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-09-25 20:09:20 +0530

    feat: Added --react --express and --next

---
+++ cli/src/helper/flagsHelper.ts
@@ -0,0 +1,34 @@
+import { CliResults } from "@/types/cli";
+import * as p from "@clack/prompts";
+import { defaultOptions } from "@/types/cli";
+
+export const flagsHelper = async ({
+  cliName,
+  isExpress,
+  isNext,
+  isReact,
+}: {
+  isReact: boolean;
+  isNext: boolean;
+  isExpress: boolean;
+  cliName: string;
+}): Promise<CliResults> => {
+  const CliPackageManager = await p.select({
+    message: "Which package manager do you want to use?",
+    options: [
+      { value: "yarn", label: "Yarn" },
+      { value: "npm", label: "npm" },
+      { value: "pnpm", label: "pnpm" },
+    ],
+    initialValue: "pnpm",
+  });
+  return {
+    turboRepoName: cliName ?? defaultOptions.turboRepoName,
+    packageManager: (CliPackageManager as "yarn" | "npm" | "pnpm") ?? "npm",
+    react: isReact ? defaultOptions.react : null,
+    next: isNext ? defaultOptions.next : null,
+    express: isExpress ? defaultOptions.express : null,
+    git: true,
+    install: true,
+  };
+};
